8420cbc651beeba59505b0f4f8249d22e296d21f
Daniel Tellez <danieltrodriguez10@gmail.com>
2026-04-07 18:41:42 -0500

    primer uso de FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,30 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return """
+    <html>
+        <head>
+            <title>First HTML</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML! - Daniel</h1>
+        </body>
+    </html>
+    """
+
+@app.get("/list")
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get("/contact") 
+def get_contact():
+    return {"name":"DANIEL TELLEZ", "email": "danieltrodriguez10@gmail.com", "phone": "+57 350"}  
+
 
 def run():
     categories = store.get_categories()
